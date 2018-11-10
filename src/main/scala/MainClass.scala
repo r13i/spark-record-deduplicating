c86e1dea51eb6b8b67cdad8febe8d3cadcbffacc
Redouane Achouri <achouri.a.r@gmail.com>
2018-11-10 16:32:44 +0100

    Exploring Spark API

--- src/main/scala/MainClass.scala
+++ src/main/scala/MainClass.scala
@@ -1,6 +1,7 @@
 // package com.example.record_deduplicate
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SparkSession, SaveMode}
+
 import org.apache.log4j.{Logger, Level}
 import com.typesafe.config.ConfigFactory
 
@@ -38,13 +39,26 @@ object MainClass {
         // println("> Number of records: " + noheader.count)
 
 
+        // We'll let Spark infer the data types, but since this requires two passes over
+        // the data, one should consider creating an instance of `StructType` and passe it
+        // to the `schema` function of the Reader API.
         val parsed = spark.read
             .option("header", "true")
             .option("nullValue", "?")
             .option("inferSchema", "true")
             .csv(conf.getString("data.path"))
+        // Also, given that the content of a DataFrame or RDD is transient, we would like to cache/persist
+        // it into memory to reduce access latency
+            .cache()       // Or use .persist(StorageLevel.MEMORY)
 
         parsed.show(5)
         parsed.printSchema()
+
+        // Save as a Parquet file
+        parsed.write.mode(SaveMode.Ignore).parquet("data/parsed-data.parquet")
+
+        // Count the matches vs non-matches
+        parsed.rdd.map(_.getAs[Boolean]("is_match")).countByValue()
+
     }
 }
