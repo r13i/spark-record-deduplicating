8a612f30c5a37bf85ea66a031e9540d3cdebe980
Redouane Achouri <achouri.a.r@gmail.com>
2018-11-18 18:45:48 +0100

    Looking for discriminative features. Work In Progress

--- src/main/scala/MainClass.scala
+++ src/main/scala/MainClass.scala
@@ -1,6 +1,7 @@
 // package com.example.record_deduplicate
 
 import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.functions.{avg, stddev}
 
 
 object MainClass extends SparkMachinery {
@@ -9,19 +10,6 @@ object MainClass extends SparkMachinery {
 
         import spark.implicits._
 
-        // val rawBlocks = spark.sparkContext.textFile(conf.getString("data.path"))
-
-        // println("> Number of rows (records + headers): " + rawBlocks.count)
-
-        // rawBlocks.take(10) foreach println
-
-        // // We use the fact that Header rows in the dataset contain the key "id_1" (among others) to distinguish between headers and and actual records
-        // def isHeader(line: String): Boolean = line.contains("id_1")
-
-        // val noheader = rawBlocks.filter(! isHeader(_))
-        // // val noheader = rawBlocks.filterNot(isHeader(_))
-
-        // println("> Number of records: " + noheader.count)
 
 
         // We'll let Spark infer the data types, but since this requires two passes over
@@ -40,11 +28,43 @@ object MainClass extends SparkMachinery {
         parsed.show(5)
         parsed.printSchema()
 
-        // Save as a Parquet file
-        parsed.write.mode(SaveMode.Ignore).parquet(conf.getString("data.parquet"))
 
-        // Count the matches vs non-matches
-        // val match_number: scala.collections.Map[Boolean, Long] = parsed.rdd.map(_.getAs[Boolean]("is_match")).countByValue()
-        parsed.groupBy("is_match").count().orderBy($"count".desc).show()
+
+        // // Save as a Parquet file
+        // parsed.write.mode(SaveMode.Ignore).parquet(conf.getString("data.parquet"))
+
+
+
+
+        // // Count the matches vs non-matches
+        // // val match_number: scala.collections.Map[Boolean, Long] = parsed.rdd.map(_.getAs[Boolean]("is_match")).countByValue()
+        // parsed.groupBy("is_match").count().orderBy($"count".desc).show()
+
+        parsed.createOrReplaceTempView("linkage")
+
+        spark.sql("""
+            SELECT is_match, COUNT(*) cnt
+            FROM linkage
+            GROUP BY is_match
+            ORDER BY cnt DESC
+        """).show()
+
+
+
+
+        // // Aggregate columns to average, standard dev, ...
+        // parsed.agg(avg($"cmp_sex"), stddev($"cmp_sex"))
+
+
+
+        // Calculate stats about the two classes (matching vs. non-matching) looking for disciminative features
+        val matches = parsed.filter($"is_match" === true)   // We use === to compare column-wise
+        val matchSummary = matches.describe()
+
+        val misses = parsed.filter($"is_match" === false)
+        val missSummary = misses.describe()
+
+        matchSummary.show()
+        missSummary.show()
     }
 }
